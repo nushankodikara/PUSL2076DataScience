5079ac597145112346bb72ec4f08e058a8c9e229
nushankodikara <nushankodi@gmail.com>
2023-09-13 16:03:15 +0530

    Completed Homework

---
+++ Lab01/Linear-Regression-Height-Weight-Genders.R
@@ -0,0 +1,10 @@
+library('ggplot2')
+library('here')
+data <- read.csv(here('Datasets','01_heights_weights_genders1.csv'))
+summary(data)
+
+ggplot(data, aes(x = Height, y = Weight, color = Gender)) +
+  geom_point() +
+  labs(title = "Scatter Plot of Height vs. Weight by Gender", x = "Height (in)", y = "Weight (lbs)") +
+  scale_color_manual(values = c("Male" = "blue", "Female" = "pink")) +
+  theme_minimal()
