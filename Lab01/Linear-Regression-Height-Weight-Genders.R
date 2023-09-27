2580a8a56a95f298aad5b91b2fc4b0175cff6468
nushankodikara <nushankodi@gmail.com>
2023-09-27 10:58:26 +0530

    First part of the lecture 03 is over

--- Lab01/Linear-Regression-Height-Weight-Genders.R
+++ Lab01/Linear-Regression-Height-Weight-Genders.R
@@ -8,3 +8,8 @@ ggplot(data, aes(x = Height, y = Weight, color = Gender)) +
   labs(title = "Scatter Plot of Height vs. Weight by Gender", x = "Height (in)", y = "Weight (lbs)") +
   scale_color_manual(values = c("Male" = "blue", "Female" = "pink")) +
   theme_minimal()
+
+# 5.Coefficients
+
+fitted.regression <- lm(Weight ~ Height, data = data)
+coef(fitted.regression)
