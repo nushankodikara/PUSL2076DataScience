03589f7fecec1c903ca551dc77c9f68954d740ea
nushankodikara <nushankodi@gmail.com>
2024-03-06 09:25:41 +0530

    Lecture 04 Start

--- Lab01/Linear-Regression-Longevity.R
+++
@@ -1,7 +0,0 @@
-library('ggplot2')
-library('here')
-data <- read.csv(here('Datasets','longevity.csv'))
-summary(data)
-
-ggplot(data, aes(x=AgeAtDeath, fill = factor(Smokes))) +
-  geom_density() + facet_grid(Smokes ~ .)
